5b348a0a80083249c3688f29d55b74ee40719634
drsh4dow <daniel.morettiv@gmail.com>
2024-10-28 12:07:18 -0300

    fix

--- lua/plugins/lsp/tailwindcss.lua
+++ lua/plugins/lsp/tailwindcss.lua
@@ -1,27 +1,29 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tailwindcss = {
-          -- exclude a filetype from the default_config
-          filetypes_exclude = { "markdown" },
-          -- add additional filetypes to the default_config
-          filetypes_include = {},
-          -- to fully override the default_config, change the below
-          -- filetypes = {}
-          settings = {
-            tailwindCSS = {
-              experimental = {
-                classRegex = {
-                  { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
-                  { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-}
+return {}
+
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         tailwindcss = {
+--           -- exclude a filetype from the default_config
+--           filetypes_exclude = { "markdown" },
+--           -- add additional filetypes to the default_config
+--           filetypes_include = {},
+--           -- to fully override the default_config, change the below
+--           -- filetypes = {}
+--           settings = {
+--             tailwindCSS = {
+--               experimental = {
+--                 classRegex = {
+--                   { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+--                   { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+--                 },
+--               },
+--             },
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
