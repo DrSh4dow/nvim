25049356000ca1090663465c535aa8ede0fd6bb1
drsh4dow <daniel.morettiv@gmail.com>
2025-05-15 19:40:17 -0400

    moved files

---
+++ lua/plugins/snacks_explorer.lua
@@ -0,0 +1,13 @@
+return {
+  -- "folke/snacks.nvim",
+  -- ---@type snacks.Config
+  -- opts = {
+  --   picker = {
+  --     sources = {
+  --       files = {
+  --         hidden = true,
+  --       }
+  --     }
+  --   }
+  -- },
+}
