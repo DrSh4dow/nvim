e60f787e4c9d5956ee866c04ced4d5902a98252b
drsh4dow <daniel.morettiv@gmail.com>
2025-07-02 17:32:00 -0400

    updated commands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "gitcommit", "tex" },
+  callback = function()
+    vim.opt_local.spell = true -- o false si quieres apagarlo
+    vim.opt_local.spelllang = { "en,es" } -- usa "en,es" para bilingüe
+  end,
+})
