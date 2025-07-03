7fbe57576bdedba5f83f6cb9c83da3e29244c5dd
drsh4dow <daniel.morettiv@gmail.com>
2025-07-03 10:29:45 -0400

    feat: add codecompanion.nvim and mcphub.nvim plugins, remove copilot-chat
    - Added `codecompanion.nvim` with mcphub extension and required
    dependencies.
    - Added `mcphub.nvim` plugin.
    - Removed `copilot-chat` extra from LazyVim configuration.
    - Updated `SchemaStore.nvim` to latest commit in lockfile.

---
+++ lua/plugins/code_companion.lua
@@ -0,0 +1,20 @@
+return {
+  "olimorris/codecompanion.nvim",
+  opts = {
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true,
+        },
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "ravitemer/mcphub.nvim",
+  },
+}
