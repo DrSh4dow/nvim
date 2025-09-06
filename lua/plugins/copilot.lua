4cf399584c7e10ac461303f5e3bd48b109b47a5a
drsh4dow <daniel.morettiv@gmail.com>
2025-09-06 11:54:47 -0400

    added copulot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,8 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
+    suggestion = { enabled = false }, -- no inline UI (Blink will show ghost text)
+    panel = { enabled = false },
     copilot_model = "gpt-5",
   },
 }
